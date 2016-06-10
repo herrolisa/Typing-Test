97b92f1a486c8cd1167862ffd89f0d809f88f2dc
Lisa <misslisazhou@gmail.com>
2016-06-09 23:57:36 -1000

    calculates wpm

--- app.js
+++ app.js
@@ -9,27 +9,35 @@ button.addEventListener("click", function () {
 
 //create "finished" event
 var finished = new Event("finished");
-document.addEventListener("finished", function () {
-  alert("You're done!!!!!!!!");
-});
 
-var elapsedTime = 0;
 //function called when start button clicked
 function start(callback) {
-  setInterval(function () {
+  //calculate how much time passed
+  var elapsedTime = 0;
+  var timer = setInterval(function () {
     console.log("setInterval" + ": " + elapsedTime++);
   }, 1000);
-  callback(err, wpm);
+
+  //listen for "finished" event on document
+  document.addEventListener("finished", function () {
+    clearInterval(timer); //stop timer
+    console.log(elapsedTime);
+    var wpm = Math.round(14/(elapsedTime/60));
+    callback(null, wpm);
+  });
+
+  //add a "keyup" event listener onto the textarea element
+  textArea.addEventListener("keyup", function (event) {
+    if (this.textLength >= 73){
+      document.dispatchEvent(finished);
+    }
+  });
 }
 
-
+//callback function
 function callback(err, wpm) {
-  //do something
-}
-
-//add a "keyup" event listener onto the textarea element
-textArea.addEventListener("keyup", function (event) {
-  if (this.textLength >= 73){
-    document.dispatchEvent(finished);
+  if (err){
+    throw new TypeError("Does not compute...");
   }
-});
+  alert("Great job! You can type " + wpm + " words per minute!");
+}
